2497b2c0d49456622b70322f7e7e00a42b8fa67d
tongyan.zjy <Leslie-2>
2023-01-18 17:58:25 +0800

    zhujiayan

--- trans_gwf_data.sh
+++ trans_gwf_data.sh
@@ -5,4 +5,4 @@ dataset=amazon-book_full
 mkdir -p data/${dataset}
 python -u trans_gwf_data.py ../HGB/Data/${dataset}/train.txt data/${dataset}/train.txt 1
 python -u trans_gwf_data.py ../HGB/Data/${dataset}/test.txt data/${dataset}/test.txt 0
-awk -F " " '{print $1"\t"$2"\t"$3}' ../HGB/Data/${dataset}/kg_final.txt data/${dataset}/kg_final.txt
+awk -F " " '{print $1"\t"$2"\t"$3}' ../HGB/Data/${dataset}/kg_final.txt > data/${dataset}/kg_final.txt
